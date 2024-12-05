3c5104c6ac3fd518d1c5464fd4da449149a2858a
VysotskySS <idgwynbleidd@gmail.com>
2024-12-05 17:18:39 +0700

    mvp new tests

--- tests/test_remote_control.py
+++ tests/test_remote_control.py
@@ -8,14 +8,14 @@ from pages.remote_control_page import RemoteControlPage
 from pages.main_page import MainPage
 
 @pytest.mark.usefixtures("setup")
-@allure.feature("Главная страница")
+@allure.feature("Удаленный доступ")
 class TestMainPage:
 
     @pytest.mark.main
     @pytest.mark.smoke
     @pytest.mark.regress
-    @allure.title('Проверка пунктов меню')
-    @allure.testcase("https://dev.corp.rudesktop.ru/-/testy/projects/2/suites/8?test_case=212")
+    @allure.title('Фильтр - Поиск устройства')
+    @allure.testcase("")
     def test_find_device(self):
         login = LoginPage()
         login.login()
