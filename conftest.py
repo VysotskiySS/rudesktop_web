4284c7d84eaf176ffca6d6a74e252c80a500c0f4
VysotskySS <idgwynbleidd@gmail.com>
2024-12-06 19:20:49 +0700

    mvp new tests

--- conftest.py
+++ conftest.py
@@ -6,14 +6,11 @@ from selenium import webdriver
 from pages.base_page import BasePage
 from selene.api import be, have, s
 
-# details_btn = s('//button[contains(.,"Дополнительные")]')
-# go_unsafe = s("//a[contains(text(), 'Перейти на сайт')]")
-
 def setup_browser(base_url):
     options = webdriver.ChromeOptions()
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-notifications")
@@ -21,14 +18,6 @@ def setup_browser(base_url):
     browser.config.timeout = 10
     browser.config.driver_options = options
     browser.open(base_url)
-    # return browser
-
-# def open_unsafe():
-#     try:
-#         details_btn.click()
-#         go_unsafe.click()
-#     except:
-#         pass
 
 def browser_teardown():
     BasePage().get_screenshot()
